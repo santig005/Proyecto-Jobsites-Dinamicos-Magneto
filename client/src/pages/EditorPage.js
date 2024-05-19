5ad29a0f9f797e293f1a2ef78953432c9646547b
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2024-05-19 18:39:47 -0500

    Agregado el componente de carrusel como plugin a GrapesJS

--- client/src/pages/EditorPage.js
+++ client/src/pages/EditorPage.js
@@ -9,18 +9,25 @@ import logoSvg from '../assets/logo-magneto.png';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import "../styles/main.css";
+import swiperComponent from "../plugins/swiper";
+
 
 function EditorPage() {
   const [editor, setEditor] = useState(null);
   useEffect(() => {
     const editor = grapesjs.init({
       container: "#editor",
-      plugins: [gjsPresetWebpage, gjsBasicBlocks, gjsForms, gjsNavbar],
+      plugins: [gjsPresetWebpage, gjsBasicBlocks, gjsForms, gjsNavbar, swiperComponent],
       pluginsOpts: {
         gjsPresetWebpage: {},
         gjsBasicBlocks: {},
         gjsForms: {},
         gjsNavbar: {},
+        swiperComponent: {},
+      },
+      canvas: {
+        styles: ['https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css'],
+        scripts: ['https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'],
       },
     });
     setEditor(editor);
@@ -58,27 +65,27 @@ function EditorPage() {
   };
 
   return (
-    <div className="App relative">
+    <div className="App">
       <nav className="bg-gray-200 py-2 px-6 flex justify-between items-center">
       <div className="logo">
         <img src={logoSvg} alt="Logo" className="h-8" />
       </div>
       <div className="buttons">
         <button
-          className="p-1 text-center text-white bg-green-500 hover:bg-green-600 border border-green-500 rounded-md active:text-green-500 hover:bg-transparent focus:outline-none focus:ring hover:scale-[1.02] transition-transform mr-4"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded mr-4"
           onClick={getHtmlAndCss}
         >
           Descargar código
         </button>
         <Link
           to="/"
-          className="p-1 text-center text-white bg-blue-500 hover:bg-blue-600 border border-blue-500 rounded-md active:text-blue-500 hover:bg-transparent focus:outline-none focus:ring hover:scale-[1.02] transition-transform mr-4"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-4"
         >
           Regresar al menú
         </Link>
         <Link
           to="/login" onClick={handleLogout} 
-          className="p-1 text-center text-white bg-red-500 hover:bg-red-600 border border-red-500 rounded-md active:text-red-500 hover:bg-transparent focus:outline-none focus:ring hover:scale-[1.02] transition-transform"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
         >
           Logout
         </Link>
