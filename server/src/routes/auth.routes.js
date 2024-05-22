42e60466eba4c84f2b3dc487fc1051565c6a9f3d
santig005 <sajhdg30@gmail.com>
2024-05-22 18:59:59 -0500

    inclusión de rutas, almacenamiento de página web y datos relacionados en bd

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,14 +1,15 @@
 import {Router} from 'express';
 import { login, register, logout, profile, verifyToken } from '../controllers/auth.controller.js';
+import { saveHtmlAndCss } from '../controllers/file.controller.js';
 import {authRequired} from '../middlewares/validateToken.js'
 const router = Router();
 
 router.post('/login', login)
 router.post('/register', register)
 router.post('/logout', logout)
-
 router.get('/verify', verifyToken)
 router.get('/profile', authRequired, profile)
+router.post('/save', saveHtmlAndCss );
 
 
 export default router
