ec0b7c9e77ce823b5cca3a29a7da37daea1a632f
santig005 <sajhdg30@gmail.com>
2024-05-24 16:56:11 -0500

    registros a partir de base de datos y botones de acciones

--- client/src/pages/AdminHomePage.js
+++ client/src/pages/AdminHomePage.js
@@ -1,13 +1,25 @@
-import React from 'react'
 import NavbarAdmin from '../components/NavbarAdmin'
 import TableAdmin from '../components/TableAdmin'
+import React, { useEffect, useState } from 'react';
+
 function AdminHomePage() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/api/admin')
+      .then(response => response.json())
+      .then(responseData => {
+        setData(responseData.pages);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
   return (
     <div>
-        <NavbarAdmin/>
-        <TableAdmin/>
+      <NavbarAdmin />
+      <TableAdmin data={data} />
     </div>
-  )
+  );
 }
 
-export default AdminHomePage
+export default AdminHomePage;
